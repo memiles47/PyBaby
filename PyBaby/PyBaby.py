630aaa946195398621bb0e7106187d121fd367f5
Michael E Miles <michael@miles-me.net>
2017-05-19 06:36:18 -0500

    Complete Script

--- PyBaby/PyBaby.py
+++ PyBaby/PyBaby.py
@@ -1,1 +1,11 @@
-#Baby talk script
+from random import choice
+
+questions = ["Why is the sky blue?: ", "Why is there a face on the moon?: ", "Where are the dinosaurs?: "]
+
+question = choice(questions)
+answer = input(question).strip().lower()
+
+while(answer != "just because"):
+    answer = input("But why? ").strip().lower()
+
+print("Oh... Okay")
